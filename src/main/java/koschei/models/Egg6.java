dfb30da827ed3b840ea4319db5952ac0fdc3cfd2
SVTsygankov <s.v.tsygankov@mail.ru>
2023-09-07 12:37:02 +0500

    Тест успешно пройден

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,9 +1,16 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Egg6 {
+
+    public Egg6(Needle7 needle) { this.needle = needle; }
+
+    @Autowired
+    private Needle7 needle;
 
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + needle.toString();
     }
 }
